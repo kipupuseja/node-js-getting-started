afb3fb8f0900fe09be91d9003773fc59c0cbcef9
kipupuseja <kipupuseja@car101.pro>
2017-09-19 13:33:51 +0000

    c1

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var request = require("request");
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -11,6 +12,21 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.get('/http/daocloud/restart/:appid/:token',function(req, res){
+    var appid = req.params.appid;
+    var token = req.params.token;
+    request({
+        method: 'POST',
+        url:"https://openapi.daocloud.io/v1/apps/"+ appid +"/actions/restart",
+        headers: {"Authorization": token}}, 
+        function (error, response, body) {
+            if(error)
+                res.send(error);
+            else
+                res.send(body);
+    });
 });
 
 app.listen(app.get('port'), function() {
